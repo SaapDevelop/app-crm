be38b7f90039f26713bf213ca82f66b9c71a20a4
SaapDevelop <saapdevelop@gmail.com>
2025-09-20 14:51:41 -0500

    Validacion de formularios con expresiones regulares

---
+++ helpers/expresiones.js
@@ -0,0 +1,4 @@
+export let expresiones = { 
+    texto:/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{5,50}$/,
+    numero: /^\d{5,10}$/
+}
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -4,15 +4,41 @@
     querySelectorAll -> Lo mas optimo (permite seleccionar cualquiera)
 
 */
+import {expresiones} from './expresiones.js';
 
 let inputs = document.querySelectorAll(".formulario__entrada")
 
 let arregloInputs = [...inputs] /*->Expansion iterable (spread operator)*/
-console.log(inputs)
-console.log(arregloInputs)
 
 arregloInputs.map((input)=> {
-    console.log(input)
+    input.addEventListener("keyup", (e)=>{
+        switch (e.target.id){
+            case "nombre":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.style.borderColor = "green" /*->En ambos casos deberia funcionar sin problema*/
+                }else {
+                    document.getElementById("nombre").style.borderColor = "red"
+                }
+                break;
+            case "apellido":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.classList.add ("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else {
+                    document.getElementById("apellido").classList.add ("incorrecto")
+                    document.getElementById("apellido").classList.remove ("correcto")
+                }
+                break;
+            case "documento":
+                if(expresiones.numero.test(e.target.value)){
+                    e.target.classList.add ("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else {
+                    document.getElementById("documento").classList.add ("incorrecto")
+                    document.getElementById("documento").classList.remove ("correcto")
+                }
+                break;
+        }
+    })
 })
 
-/*inputs.map(function(){})*/
